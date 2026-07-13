af045ba5b34a9e79510c300a6aa596db7dec9799
s4195745 <s4195745@rmit.edu.vn>
2026-07-14 00:50:27 +0700

    2 cart added

---
+++ game-merch/modules/shopping_cart/products.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        id: 1,
+        title: "The Knight Plush",
+        price: "$32",
+        description: "The hollow knight from the Hollow Knight",
+        colors: [
+            {
+                colorName: "black",
+                mainImage: "../../assets/cart.images/product_HK_knight_plush_main.webp",
+                thumbnails: [
+                    "../../assets/cart.images/product_HK_knight_plush_photo1.webp",
+                    "../../assets/cart.images/product_HK_knight_plush_photo2.webp",
+                    "../../assets/cart.images/product_HK_knight_plush_photo3.webp"
+                ],
+                sizes: ["S", "M", "L"]
+            }
+        ]
+    },
+
+        {
+        id: 2,
+        title: "Hornet Plush",
+        price: "$32",
+        description: "Hornet from Silksong",
+        colors: [
+            {
+                colorName: "red",
+                mainImage: "../../assets/cart.images/product_HK_hornet_plush_main.webp",
+                thumbnails: [
+                    "../../assets/cart.images/product_HK_hornet_plush_photo1.webp",
+                    "../../assets/cart.images/product_HK_hornet_plush_photo2.webp",
+                ],
+                sizes: ["S", "M", "L"]
+            }
+        ]
+    },
+
+];
