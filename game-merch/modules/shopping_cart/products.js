fa3c6ef4e09e68d20397a13258125cb9a1df4fc4
s4195745 <s4195745@rmit.edu.vn>
2026-07-21 08:38:29 +0700

    images update

--- game-merch/modules/shopping_cart/products.js
+++ game-merch/modules/shopping_cart/products.js
@@ -1,58 +1,86 @@
 const products = [
-    
     {
         id: 1,
-        title: "The Knight Plush",
+        title: "Hollow Knight",
         price: "$32",
-        description: "The hollow knight from the Hollow Knight",
-        
-        mainImage: "../../assets/cart.images/product_HK_knight_plush_main.webp",
-        thumbnails: [
-            "../../assets/cart.images/product_HK_knight_plush_photo1.webp",
-            "../../assets/cart.images/product_HK_knight_plush_photo2.webp",
-            "../../assets/cart.images/product_HK_knight_plush_photo3.webp"
-        ],
-        sizes: ["S", "M", "L","XL","XXL"]
-    
+        description: "Find a home for the Knight that's a little less dangerous than Hallownest.",
+        colors: [
+            {
+                name: "White",
+                mainImage: "../../assets/cart.images/white/white_hk_main.webp",
+                thumbnails: [
+                    "../../assets/cart.images/white/white_hk_photo1.webp",
+                    "../../assets/cart.images/white/white_hk_photo2.webp",
+                    "../../assets/cart.images/white/white_hk_photo3.webp"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            },
+            {
+                name: "Yellow",
+                mainImage: "../../assets/cart.images/yellow/yellow_hk_main.jpg",
+                thumbnails: [
+                    "../../assets/cart.images/yellow/yellow_hk_photo1.jpg",
+                    "../../assets/cart.images/yellow/yellow_hk_photo2.jpg",
+                    "../../assets/cart.images/yellow/yellow_hk_photo3.jpg"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            },
+            {
+                name: "Red",
+                mainImage: "../../assets/cart.images/red/red_hk_main.jpg",
+                thumbnails: [
+                    "../../assets/cart.images/red/red_hk_photo1.jpg",
+                    "../../assets/cart.images/red/red_hk_photo1.jpg",
+                    "../../assets/cart.images/red/red_hk_photo1.jpg"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            },
+            {
+                name: "Purple",
+                mainImage: "../../assets/cart.images/purple/purple_hk_main.jpg",
+                thumbnails: [
+                    "../../assets/cart.images/purple/purple_hk_photo1.jpg",
+                    "../../assets/cart.images/purple/purple_hk_photo2.jpg",
+                    "../../assets/cart.images/purple/purple_hk_photo3.jpg"
+                ],
+                sizes: ["S", "L"]
+            },
+            {
+                name: "Black",
+                mainImage: "../../assets/cart.images/black/black_hk_main.jpg",
+                thumbnails: [
+                    "../../assets/cart.images/black/black_hk_photo1.jpg",
+                    "../../assets/cart.images/black/black_hk_photo2.jpg",
+                    "../../assets/cart.images/black/black_hk_photo3.jpg"
+                ],
+                sizes: ["M", "L", "XL"]
+            }
+        ]
     },
-
-
     {
         id: 2,
         title: "Hornet Plush",
         price: "$32",
         description: "Hornet from Silksong",
-        colors: [
-            {
-                colorName: "red",
-                mainImage: "../../assets/cart.images/product_HK_hornet_plush_main.webp",
-                thumbnails: [
-                    "../../assets/cart.images/product_HK_hornet_plush_photo1.webp",
-                    "../../assets/cart.images/product_HK_hornet_plush_photo2.webp",
-                ],
-                sizes: ["S", "M", "L","XL","XXL"]
-            }
-        ]
+        mainImage: "../../assets/cart.images/product_HK_hornet_plush_main.webp",
+        thumbnails: [
+            "../../assets/cart.images/product_HK_hornet_plush_photo1.webp",
+            "../../assets/cart.images/product_HK_hornet_plush_photo2.webp",
+            "../../assets/cart.images/product_HK_hornet_plush_photo3.webp" 
+        ],
+        sizes: ["S", "M", "L", "XL", "XXL"]
     },
-
-
     {
         id: 3,
         title: "Grim Plush",
         price: "$32",
         description: "Grim from Silksong",
-        colors: [
-            {
-                colorName: "red",
-                mainImage: "../../assets/cart.images/product_HK_grimm_plush_main.webp",
-                thumbnails: [
-                    "../../assets/cart.images/product_HK_grimm_plush_wire.webp",
-                    "../../assets/cart.images/product_HK_grimm_plush_photo3.webp",
-                ],
-                sizes: ["S", "M", "L","XL","XXL"]
-            }
-        ]
-    },
-
-
+        mainImage: "../../assets/cart.images/product_HK_grimm_plush_main.webp",
+        thumbnails: [
+            "../../assets/cart.images/product_HK_grimm_plush_wire.webp",
+            "../../assets/cart.images/product_HK_grimm_plush_photo3.webp",
+            "../../assets/cart.images/product_HK_grimm_plush_photo4.webp"
+        ],
+        sizes: ["S", "M", "L", "XL", "XXL"]
+    }
 ];
