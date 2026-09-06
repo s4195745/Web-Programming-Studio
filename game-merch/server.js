25c9e0be8e0ec250a2faea06cdc9336ba49997d5
s4197975 <s4197975@rmit.edu.vn>
2026-09-06 12:09:39 +0700

    final

--- game-merch/server.js
+++ game-merch/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const PORT = 3000;
